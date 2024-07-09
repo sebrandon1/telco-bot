8c9f98807b9a8916240c2853f5bca46a670201c3
Brandon Palm <bpalm@redhat.com>
2024-07-09 10:31:46 -0500

    Fix the script

--- scripts/send-slack-msg.sh
+++ scripts/send-slack-msg.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # Slack Incoming Webhook URL
-SLACK_WEBHOOK_URL=$2
-JSON_FILE=$3
-DAYS_BACK=$4
+SLACK_WEBHOOK_URL=$1
+JSON_FILE=$2
+DAYS_BACK=$3
 
 NUMBER_OF_RECORDS=$(cat $JSON_FILE | jq '.jobs | length')
 
-MESSAGE="There have been $NUMBER_OF_RECORDS DCI jobs in the last $DAYS_BACK days.\n"
+MESSAGE="There have been $NUMBER_OF_RECORDS DCI jobs in the last $DAYS_BACK days."
 
+echo $MESSAGE
+
+DATA="{\"message\"   : \"${MESSAGE}\"}"
+
+# Send the message to Slack
+curl -X POST -H 'Content-type: application/json charset=UTF-8' --data "$DATA" $SLACK_WEBHOOK_URL
