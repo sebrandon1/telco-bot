b0c1a54890e32b4cbacb3d6cdf9e323e10e5f5cd
Brandon Palm <bpalm@redhat.com>
2024-07-09 11:38:21 -0500

    Add version counts

--- scripts/send-slack-msg.sh
+++ scripts/send-slack-msg.sh
@@ -7,7 +7,20 @@ DAYS_BACK=$3
 
 NUMBER_OF_RECORDS=$(cat $JSON_FILE | jq '.jobs | length')
 
-MESSAGE="There have been $NUMBER_OF_RECORDS DCI jobs in the last $DAYS_BACK days."
+MESSAGE="There have been $NUMBER_OF_RECORDS DCI jobs that have used the certsuite in the last $DAYS_BACK days.\n"
+
+VERSIONS_BY_VALUE=$(cat $JSON_FILE | jq '.jobs | group_by(.tnf_version) | map({key: .[0].tnf_version, value: length})')
+
+for row in $(echo "${VERSIONS_BY_VALUE}" | jq -r '.[] | @base64'); do
+    _jq() {
+        echo ${row} | base64 --decode | jq -r ${1}
+    }
+
+    VERSION=$(_jq '.key')
+    COUNT=$(_jq '.value')
+
+    MESSAGE="$MESSAGE\n Version: $VERSION -- Run Count: $COUNT"
+done
 
 echo $MESSAGE
 
