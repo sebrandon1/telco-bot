108d584cf0f554a8702161182e1274e741a04042
Brandon Palm <bpalm@redhat.com>
2024-07-10 12:16:18 -0500

    Adjust for commit hashes

--- scripts/send-slack-msg.sh
+++ scripts/send-slack-msg.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+SEMVER_REGEX="^v[0-9]+\.[0-9]+\.[0-9]+$"
+
 # Slack Incoming Webhook URL
 SLACK_WEBHOOK_URL=$1
 JSON_FILE=$2
 DAYS_BACK=$3
 
 NUMBER_OF_RECORDS=$(cat $JSON_FILE | jq '.jobs | length')
+
+RUNS_BY_COMMIT_CTR=0
 
 MESSAGE="There have been $NUMBER_OF_RECORDS DCI jobs that have used the certsuite in the last $DAYS_BACK days.\n"
 
@@ -16,11 +20,20 @@ for row in $(echo "${VERSIONS_BY_VALUE}" | jq -r '.[] | @base64'); do
         echo ${row} | base64 --decode | jq -r ${1}
     }
 
-    VERSION=$(_jq '.key')
-    COUNT=$(_jq '.value')
+    # Check if the version matches the semver regex
+    if [[ ! $(_jq '.key') =~ $SEMVER_REGEX ]]; then
+        let "RUNS_BY_COMMIT_CTR=RUNS_BY_COMMIT_CTR+1"
+    else
+        VERSION=$(_jq '.key')
+        COUNT=$(_jq '.value')
 
-    MESSAGE="$MESSAGE\n Version: $VERSION -- Run Count: $COUNT"
+        MESSAGE="$MESSAGE\n Version: $VERSION -- Run Count: $COUNT"
+    fi
 done
+
+if [ $RUNS_BY_COMMIT_CTR -gt 0 ]; then
+    MESSAGE="$MESSAGE\n\nThere have been $RUNS_BY_COMMIT_CTR runs by commit hash."
+fi
 
 echo $MESSAGE
 
